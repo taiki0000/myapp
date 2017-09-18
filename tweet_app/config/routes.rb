b1adec71f5b6ed7ce448bfcb247dc44ae34af891
[taiki0000] <[nikora0425@icloud.com]>
2017-09-19 01:15:58 +0900

    3が終了

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'posts/create' => "posts#create"
+  get 'posts/new' => "posts#new"
   get 'posts/index' => "posts#index"
   get 'posts/:id' => "posts#show"
 
