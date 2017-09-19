abda2ea7065d7e41c9cc5e386587fd1f8c1d513a
T.kawa <nikora0425@icloud.com>
2017-09-19 18:29:23 +0900

    4が終わった．編集，除去ができる．

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   post 'posts/create' => "posts#create"
   post 'posts/new' => "posts#new"
+  post 'posts/:id/update' => "posts#update"
+  post 'posts/:id/destroy' => "posts#destroy"
   get 'posts/new' => "posts#new"
   get 'posts/index' => "posts#index"
   get 'posts/:id' => "posts#show"
+  get 'posts/:id/edit' => "posts#edit"
+
 
   get '/' => "home#top"
   get 'about' => "home#about"
