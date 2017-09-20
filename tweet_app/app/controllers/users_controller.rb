1bcff48b9ea4f4e0e5d580a98f166c6a2424ef74
[taiki0000] <[nikora0425@icloud.com]>
2017-09-21 01:25:32 +0900

    7が終わった．プロフィール画像が投稿できる

--- tweet_app/app/controllers/users_controller.rb
+++ tweet_app/app/controllers/users_controller.rb
@@ -9,8 +9,12 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    @user = User.new(name:params[:name],email:params[:email])
-    if @user.save == true
+    @user = User.new(
+        name:params[:name],
+        email:params[:email],
+        image_name:"defa.png"
+    )
+    if @user.save
       redirect_to("/users/#{@user.id}")
       flash[:notice] = "登録ができました"
     else
@@ -21,11 +25,18 @@ class UsersController < ApplicationController
     @user = User.find_by(id:params[:id])
   end
   def update
+
     @user = User.find_by(id:params[:id])
     @user.name = params[:name]
     @user.email =params[:email]
 
-    if @user.save == true
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}",image.read)
+    end
+
+    if @user.save
       redirect_to("/users/#{@user.id}")
       flash[:notice] = "ユーザー情報を編集しました"
     else
